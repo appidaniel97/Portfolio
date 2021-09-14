f34433dcece42432c7a17cf7ec09fd9d22a1a733
appidaniel97 <appidaniel97@gmail.com>
2021-09-13 22:29:49 -0300

    Estrutura Inicial dos Arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+var hamburguer = document.querySelector(".Menuhamburger");
+
+hamburguer.addEventListener("click", function()
+{
+    document.querySelector(".container").classList.toggle("show-menu");
+});
